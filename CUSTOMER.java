cc57840687b55219577d5327c79f79043d2e566f
claudia123456 <Guest@stk-cs01-ws12.westlothian.sch.uk>
2015-12-16 11:10:24 +0000

    sprint 1

---
+++ CUSTOMER.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class CUSTOMER here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CUSTOMER
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class CUSTOMER
+     */
+    public CUSTOMER()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
